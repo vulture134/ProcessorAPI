51b6efe34025039d3ad578b8566af5a6d6d5316b
vulture <eradicator322@mail.ru>
2022-05-16 12:19:20 +0400

    vulture

--- build.sbt
+++ build.sbt
@@ -25,4 +25,5 @@ libraryDependencies ++= Seq(
   "org.slf4j" % "slf4j-simple" % "1.7.36",
   "org.slf4j" % "slf4j-api" % "1.7.36",
   "org.slf4j" % "slf4j-simple" % "1.7.36"
+
 )
--- src/main/scala/WordCountProcessor.scala
+++ src/main/scala/WordCountProcessor.scala
@@ -1,4 +1,5 @@
 import org.apache.kafka.streams.processor.api.Processor
+import org.apache.kafka.streams.processor.api.Record
 import org.apache.kafka.streams.processor.{ProcessorContext, PunctuationType}
 import org.apache.kafka.streams.scala.serialization.Serdes
 import org.apache.kafka.streams.state.{KeyValueIterator, KeyValueStore, StoreBuilder, Stores}
@@ -10,7 +11,7 @@ import java.util.{Locale, Properties}
 
 class WordCountProcessor extends Processor[String, String, String, String]{
 
-  var kvStore: KeyValueStore[String, Int] = null
+  var kvStore: KeyValueStore[String, Int] = _
 
   def init (context: ProcessorContext): Unit = {
     context.schedule(Duration.ofSeconds(1), PunctuationType.STREAM_TIME, _ => {
@@ -23,48 +24,59 @@ class WordCountProcessor extends Processor[String, String, String, String]{
     kvStore = context.getStateStore("Counts")
   }
 
-  def process (record: Record): Unit = {
-    val words: List[String] = record.value().toLowerCase(Locale.getDefault()).split("\\W+")
+  def process (record: Record[String, String]): Unit = {
+    val words: List[String] = record.value().toLowerCase(Locale.getDefault()).split("\\W+").toList
     for (word <- words) {
       val oldValue: Int = kvStore.get(word)
       if (oldValue == 0) kvStore.put(word, 1) else kvStore.put(word, oldValue + 1)
     }
   }
 
-  def close(): Unit
+  override def close(): Unit = ()
 }
 
-class EnrichProcessor extends Processor[String, String] {
+class EnrichProcessor extends Processor[String, String, Unit, Unit] {
 
-  var context: ProcessorContext
+  var context: ProcessorContext = _
 
   def init (context: ProcessorContext): Unit = {
     this.context = context
   }
 
+  def process (record: Record [String, String]): Unit = {
+    context.topic() match {
+      case _ => context.forward (record.key, record.value)
+    }
+  }
+  override def close(): Unit = ()
+
 }
 
-val countStoreSupplier: StoreBuilder[KeyValueStore[String, Long]] = Stores.keyValueStoreBuilder(
-  Stores.persistentKeyValueStore("Counts"),
-  Serdes.stringSerde,
-  Serdes.longSerde)
-  .withLoggingDisabled()
+object Main extends App {
 
-val topology: Topology = new Topology
+  val countStoreSupplier: StoreBuilder[KeyValueStore[String, Long]] = Stores.keyValueStoreBuilder(
+    Stores.persistentKeyValueStore("Counts"),
+    Serdes.stringSerde,
+    Serdes.longSerde)
+    .withLoggingDisabled()
 
-topology.addSource("Source", "source-topic")
-  .addProcessor("Process", () => new WordCountProcessor(), "Source")
-  .addStateStore(countStoreSupplier, "Process")
-  .addSink("Sink","sink-topic","Process")
+  val topology: Topology = new Topology
 
-
-val properties: Properties = new Properties()
-properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "processor-api-application")
-properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  topology.addSource("Source", "source-topic")
+    .addProcessor("Process", () => new WordCountProcessor(), "Source")
+    .addStateStore(countStoreSupplier, "Process")
+    .addSink("Sink","sink-topic","Process")
 
 
-val KafkaStreams = new KafkaStreams(topology, properties)
-KafkaStreams.start()
-sys.ShutdownHookThread {
-  KafkaStreams.close(Duration.ofSeconds(10))
+  val properties: Properties = new Properties()
+  properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "processor-api-application")
+  properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.stringSerde.getClass)
+  properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.stringSerde.getClass)
+
+  val KafkaStreams = new KafkaStreams(topology, properties)
+  KafkaStreams.start()
+  sys.ShutdownHookThread {
+    KafkaStreams.close(Duration.ofSeconds(10))
+  }
 }
